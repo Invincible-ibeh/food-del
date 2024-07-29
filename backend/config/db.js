b5427754929abbc0e0d8d4300f0c5644db77e534
Ibeh Miracle <miradmayor@gmail.com>
2024-07-28 20:52:02 -0400

    Created the Admin Panel

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+
 export const connectDB = async () => {
     await mongoose.connect(
       'mongodb+srv://miradmayor:UC6KPQQeo8llaHrgibehm@cluster0.vf7rcnw.mongodb.net/food-del'
