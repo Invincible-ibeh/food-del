8de786e16e2deead855c1bb1392f4833583725ad
Ibeh Miracle <miradmayor@gmail.com>
2024-07-30 03:02:54 -0400

    Added env file to gitignore

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,6 @@ const dbURI = process.env.MONGO_URI; // Fetch the MongoDB URI
 
 export const connectDB = async () => {
     await mongoose.connect(
-      'mongodb+srv://miradmayor:UC6KPQQeo8llaHrgibehm@cluster0.vf7rcnw.mongodb.net/food-del'
+      'dbURI'
     ).then(()=>console.log("DB Connected"));
 }
