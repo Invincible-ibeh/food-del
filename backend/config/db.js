07a45d284112a1ffce32887b22ee1c8dfc7783fe
Ibeh Miracle <miradmayor@gmail.com>
2024-07-30 02:58:23 -0400

    Added env file to gitignore

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+require('dotenv').config(); // Read the secrets from the scroll
+const dbURI = process.env.MONGO_URI; // Fetch the MongoDB URI
+
 
 
 export const connectDB = async () => {
