523bce89f855ad67076216911892d7056d1bc30f
Ibeh Miracle <miradmayor@gmail.com>
2024-08-13 05:34:37 -0400

    Modified the Backend Server

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,3 @@
-// import mongoose from "mongoose";
-
-// import dotenv from "dotenv";
-// dotenv.config();
-
-
-// const dbURI = process.env.MONGO_URI; // Fetch the MongoDB URI
-
-
-
-
-// export const connectDB = async () => {
-//     await mongoose.connect(
-//       'dbURI'
-//     ).then(()=>console.log("DB Connected"));
-// }
-
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
@@ -30,4 +13,3 @@ export const connectDB = async () => {
     .then(() => console.log("DB Connected"))
     .catch((error) => console.error("DB Connection Error:", error));
 };
-
