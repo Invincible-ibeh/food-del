04ba64ca60a0c4d2f3bcbb84218949ccb57e39d6
Ibeh Miracle <miradmayor@gmail.com>
2024-08-13 06:14:36 -0400

    Added Database Config

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const dbURI = process.env.MONGO_URI; // Fetch the MongoDB URI
+
+export const connectDB = async () => {
+  await mongoose
+    .connect(
+      dbURI // Use the actual dbURI variable here
+    )
+    .then(() => console.log("DB Connected"))
+    .catch((error) => console.error("DB Connection Error:", error));
+};
