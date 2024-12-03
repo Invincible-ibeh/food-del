9b37a9852f417908dd60fb07055ac604db7671ab
Ibeh Miracle <miradmayor@gmail.com>
2024-12-03 06:41:55 -0800

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //Placing User Order From Frontend
 const placeOrder = async (req, res) => {
 
-    const frontend_url = "http://localhost:5174";
+    const frontend_url = "https://food-del-frontend-6pj9.onrender.com/";
     
     try {
         const newOrder = new orderModel({
